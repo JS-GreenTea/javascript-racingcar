d68bb549251812102c00bd4fbaf670a573214dbc
spiderq2697 <spiderq10@gmail.com>
2022-06-24 22:21:12 +0900

    feat(Model): trialCount를 저장한다

--- src/model/RacingModel.js
+++ src/model/RacingModel.js
@@ -1,16 +1,30 @@
 class RacingModel {
   #racingCars;
+  #trialCount;
 
   constructor() {
     this.#racingCars = [];
+    this.#trialCount = 0;
   }
 
   get racingCars() {
     return [...this.#racingCars];
   }
 
+  get trialCount() {
+    return this.#trialCount;
+  }
+
+  set trialCount(count) {
+    this.#trialCount = count;
+  }
+
   addCar(carName) {
     this.#racingCars.push(carName);
+  }
+
+  reduceTrialCount() {
+    this.#trialCount -= 1;
   }
 }
 
