c3a446b92371201c4c9e87e16aec4aacaf81afd0
KINHYEONJI <hyeonji0718@gmail.com>
2022-07-01 15:58:59 +0900

    feat(racingcar): 차가 전진하는 로직 구현
    - 전진하는 조건을 검사하는 함수 isAccelValid 구현
    - 전진 여부에 따라 돔을 조작하는 함수 accelCar 구현

--- src/racingcar.js
+++ src/racingcar.js
@@ -1,4 +1,11 @@
 export default class Racingcar {
+  constructor(carOrder, carName, raceCount) {
+    this.carOrder = carOrder;
+    this.carName = carName;
+    this.raceCount = raceCount;
+    this.moveCount = 0;
+  }
+
   findParentDiv(divClassName) {
     return document.getElementsByClassName(`${divClassName}`)[0];
   }
@@ -16,5 +23,21 @@ export default class Racingcar {
     this.carNameDiv = this.createChildDiv("carNameLabel", carName);
     this.racingResultDiv.append(this.carDiv);
     this.carDiv.append(this.carNameDiv);
+  }
+
+  accelCar(raceCount) {
+    this.racingResultDiv = this.findParentDiv("racingResult");
+    this.carDiv = document.getElementsByClassName("car")[this.carOrder];
+    for (let i = 0; i < raceCount; i += 1) {
+      if (this.isAccelValid()) {
+        this.countDiv = this.createChildDiv("count", "⬇️");
+        this.carDiv.append(this.countDiv);
+        this.moveCount = this.moveCount + 1;
+      }
+    }
+  }
+
+  isAccelValid() {
+    return Math.floor(Math.random() * 9) >= 4;
   }
 }
