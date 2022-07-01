2a0c1e73658076d3c0d28b9de5fb78d7aa7f68e7
spiderq2697 <spiderq10@gmail.com>
2022-07-01 10:46:03 +0900

    fix: 자동차 이름에 대한 유효성 검사 조건을 수정한다
    - 5자 이상의 자동차 이름에 대한 에러처리를 6자 이상의 자동차 이름에 대한 에러처리로 변경한다

--- src/validation.js
+++ src/validation.js
@@ -1,5 +1,5 @@
 const isLengthLessThen = (arr, length) => {
-  return !arr.find(value => value.length > length);
+  return !arr.find(value => value.length >= length);
 };
 
 export { isLengthLessThen };
