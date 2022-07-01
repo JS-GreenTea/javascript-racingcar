cde622eb7cb050a7ba121e50fe7f1a496362ed09
KINHYEONJI <hyeonji0718@gmail.com>
2022-07-01 15:50:48 +0900

    feat(racingcargame): 자동차 이름 입력 값 유효성 검사
    - 5대 이하인지 확인
    - 이름의 길이가 5 이하인지 확인

--- src/racingcargame.js
+++ src/racingcargame.js
@@ -6,4 +6,25 @@ export default class RacingCarGame {
     this.carNameList = String(carNameListInput).split(",");
     return this.carNameList;
   }
+
+  isCarNameListInputValid(carNameListInput) {
+    this.carNameList = this.splitCarNameListInput(carNameListInput);
+    console.log(this.carNameList);
+
+    if (this.carNameList.length > 5) {
+      alert("5대 이하의 자동차를 입력해주세요.");
+      location.reload();
+      return false;
+    }
+
+    this.carNameList.forEach((carName) => {
+      if (carName.length > 5) {
+        alert("차 이름은 5자 이하로 입력해주세요.");
+        location.reload();
+        return false;
+      }
+    });
+
+    return true;
+  }
 }
